2a3b781e2efab9df1762a5bf086606903da25ca8
Anishkolkata <anish.rcciit@gmail.com>
2025-06-07 19:51:32 +0530

    application level config: added in memory users wih roles as a configuration file

---
+++ src/main/java/com/authentication/BasicAuthExample/InMemoryUserCreationWithRolesConfig.java
@@ -0,0 +1,28 @@
+package com.authentication.BasicAuthExample;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class InMemoryUserCreationWithRolesConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User
+                .withUsername("user1")
+                .password("{noop}password1") // {noop} indicates no password encoder is used
+                .roles("USER")
+                .build();
+        UserDetails user2 = User
+                .withUsername("admin")
+                .password("{noop}password2") // {noop} indicates no password encoder is used
+                .roles("ADMIN")
+                .build();
+        //InMemoryUserDetailsService implements the interface UserDetailsService
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+}
