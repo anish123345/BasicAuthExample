1f26fb7df8b774a5c42a616176b9661d2a0097a8
Anishkolkata <anish.rcciit@gmail.com>
2025-06-07 19:53:13 +0530

    method level @Preauthorize in controller.

--- src/main/java/com/authentication/BasicAuthExample/HelloController.java
+++ src/main/java/com/authentication/BasicAuthExample/HelloController.java
@@ -1,5 +1,6 @@
 package com.authentication.BasicAuthExample;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,16 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize(("hasRole('ADMIN')"))
+    public String admin() {
+        return "Hello, Admin!";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "Hello, User!";
     }
 }
