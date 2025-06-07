8f51ab5546e7d2d3d940ef39b9955f5bb8ba1bd4
Anishkolkata <anish.rcciit@gmail.com>
2025-06-07 15:28:40 +0530

    initial commit

---
+++ src/main/java/com/authentication/BasicAuthExample/BasicAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.BasicAuthExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentication/BasicAuthExample/BasicAuthExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.BasicAuthExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicAuthExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
