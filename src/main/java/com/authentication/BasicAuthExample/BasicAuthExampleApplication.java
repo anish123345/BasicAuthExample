e1687c4d7314eac34a171aabd81164759bf5f797
Anishkolkata <anish.rcciit@gmail.com>
2025-06-08 16:58:19 +0530

    git check comment

--- src/main/java/com/authentication/BasicAuthExample/BasicAuthExampleApplication.java
+++ src/main/java/com/authentication/BasicAuthExample/BasicAuthExampleApplication.java
@@ -9,6 +9,7 @@ public class BasicAuthExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BasicAuthExampleApplication.class, args);
 		//Running this application will start the Spring Boot application
+		//This is the entry class
 	}
 
 }
