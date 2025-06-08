462800e57b2088e4ed2f8b6a7ec970e292eebc88
Anishkolkata <anish.rcciit@gmail.com>
2025-06-08 12:49:52 +0530

    Configuration: Implementing password encoder BCrypt.Now, passwords getting stored in DB in encrypted format

--- src/main/java/com/authentication/BasicAuthExample/UserCreationStoredInH2Database.java
+++ src/main/java/com/authentication/BasicAuthExample/UserCreationStoredInH2Database.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
@@ -17,16 +19,26 @@ public class UserCreationStoredInH2Database {
     @Autowired
     DataSource dataSource;
 
+    /**
+     * Configures and returns a UserDetailsService bean.
+     *
+     * This method creates two users with encoded passwords and assigns them roles.
+     * The users are stored in an H2 database using JdbcUserDetailsManager, which implements
+     * the UserDetailsService interface. This allows Spring Security to retrieve user details
+     * for authentication and authorization purposes.
+     *
+     * @return UserDetailsService - an instance of JdbcUserDetailsManager configured with the DataSource
+     */
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User
                 .withUsername("user1")
-                .password("{noop}password1") // {noop} indicates no password encoder is used
+                .password(passwordEncoder().encode("password1"))// {noop} not used now. We are encoding and saving to DB
                 .roles("USER")
                 .build();
         UserDetails user2 = User
                 .withUsername("admin")
-                .password("{noop}password2") // {noop} indicates no password encoder is used
+                .password(passwordEncoder().encode("password2")) // {noop} not used now. We are encoding and saving to DB
                 .roles("ADMIN")
                 .build();
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
@@ -34,5 +46,17 @@ public class UserCreationStoredInH2Database {
         jdbcUserDetailsManager.createUser(user2);
         //JdbcUserDetailsManager implements the interface UserDetailsService
         return jdbcUserDetailsManager;
+    }
+
+    /**
+     * Creates and returns a PasswordEncoder bean.
+     *
+     * This method provides an instance of BCryptPasswordEncoder, which is used to encode passwords
+     * securely using the BCrypt hashing algorithm. The encoded passwords can then be stored in the database.
+     *
+     * @return PasswordEncoder - an instance of BCryptPasswordEncoder
+     */
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
