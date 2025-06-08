fb659dba37c183e9cfb19f26ed16c95b9d05fe46
Anishkolkata <anish.rcciit@gmail.com>
2025-06-08 11:54:12 +0530

    Configuration: h2 added and configured.

--- src/main/java/com/authentication/BasicAuthExample/CustomSecurityFilter.java
+++ src/main/java/com/authentication/BasicAuthExample/CustomSecurityFilter.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,11 +18,16 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class CustomSecurityFilter {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.authorizeHttpRequests((requests) ->
+                requests.requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated());
         http.sessionManagement(
                 session -> session.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS)
         );
         //http.formLogin(withDefaults()); // Uncomment this line to enable form-based login
+        http.headers(headers ->
+                headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));// As frame for h2 console were not coming, we did this.
+        http.csrf(csrf -> csrf.disable()); // Disable CSRF protection for H2 console
         http.httpBasic(withDefaults());
         return http.build();
     }
