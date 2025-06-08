e2b33ca880923c7b5c4358fe11f6592f970d2fbb
Anishkolkata <anish.rcciit@gmail.com>
2025-06-08 12:37:32 +0530

    code suggestion to use lambda for AbstractHttpConfigurer

--- src/main/java/com/authentication/BasicAuthExample/CustomSecurityFilter.java
+++ src/main/java/com/authentication/BasicAuthExample/CustomSecurityFilter.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,7 +28,7 @@ public class CustomSecurityFilter {
         //http.formLogin(withDefaults()); // Uncomment this line to enable form-based login
         http.headers(headers ->
                 headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));// As frame for h2 console were not coming, we did this.
-        http.csrf(csrf -> csrf.disable()); // Disable CSRF protection for H2 console
+        http.csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection for H2 console
         http.httpBasic(withDefaults());
         return http.build();
     }
